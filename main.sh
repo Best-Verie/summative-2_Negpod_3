9cb630fde18037eb998594a8c0f903c27841845f
Habib Josue Ahadi <h.ahadi1@alustudent.com>
2024-02-13 15:31:21 +0200

    Prompt user for student details

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+echo "Welcome to the ALU Student Registration System!"
+
+# Prompt user for student detaisl
+read -p "Enter student email: " email
+read -p "Enter student age: " age
+read -p "Enter student ID: " student_id
