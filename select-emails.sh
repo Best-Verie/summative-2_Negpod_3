6a8ed677a6d6d23c41cd5d4144109c665d97dd8a
Josue <ahadihjosue@gmail.com>
2024-02-13 17:04:38 +0200

    emails in the student-emails.txt file are sorted alphabetically every time a new student is added

--- select-emails.sh
+++ select-emails.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Function to select emails of students and save them to student-emails.txt
+# Function to select emails of students, sort them alphabetically, and save them to student-emails.txt
 select_emails() {
-    grep -oP '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b' students-list_1023.txt > student-emails.txt
-    echo "Emails of students have been selected and saved to student-emails.txt"
+    grep -oP '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b' students-list_1023.txt | sort > student-emails.txt
+    echo "Emails of students have been selected and saved to student-emails.txt (sorted alphabetically)"
 }
 
 # Call the select_emails function
