3c4e5f3a6e2d3a993b1086f10e75a3f94df16368
Josue <ahadihjosue@gmail.com>
2024-02-13 19:45:50 +0200

    backup functionality

--- backup-Negpod_3.sh
+++ backup-Negpod_3.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # Remote server details
-remote_host="2f05c1f8800b.3be8ebfc.alu-cod.online"
-remote_username="2f05c1f8800b"
-remote_password="d4a1d225d0abda9549d8"
+host="2f05c1f8800b.3be8ebfc.alu-cod.online"
+username="2f05c1f8800b"
+password="d4a1d225d0abda9549d8"
 remote_directory="/summative/1023-2024j"
 
-# Copy the directory to the remote server
-scp -r negpod_3-q1 "$remote_username@$remote_host:$remote_directory"
+# Backup directory
+local_directory="negpod_3-q1"
 
-echo "Backup completed successfully to $remote_host:$remote_directory."
+# Perform backup using rsync
+rsync -avz --progress -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" "$local_directory" "$username@$host:$remote_directory"
+
+# Check if backup is successful
+if sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$host" "[ -d \"$remote_directory/$local_directory\" ]"; then
+    echo "Backup completed successfully."
+else
+    echo "Error: Backup failed."
+fi
