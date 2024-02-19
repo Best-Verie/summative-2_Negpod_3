0c5bd0730d158cb4e1048884fec6c37e9557b816
Habib Josue Ahadi <h.ahadi1@alustudent.com>
2024-02-19 10:04:20 +0200

    remote server updated

--- backup-Negpod_3.sh
+++ backup-Negpod_3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Remote server details
-host="2f05c1f8800b.3be8ebfc.alu-cod.online"
-username="2f05c1f8800b"
-password="d4a1d225d0abda9549d8"
+host="64293e56bc62.3a2627c1.alu-cod.online"
+username="64293e56bc62"
+password="328d3b338a4ced526c9a"
 remote_directory="/summative/1023-2024j"
 
 # Backup directory
